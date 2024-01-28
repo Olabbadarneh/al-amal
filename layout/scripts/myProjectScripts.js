edf1985c2d7a4af2312c4e45a8610d7464934e2f
Ola Badarneh <o.badarneh@ats-ware.com>
2024-01-28 10:13:36 +0300

    my project

--- layout/scripts/myProjectScripts.js
+++ layout/scripts/myProjectScripts.js
@@ -4,3 +4,28 @@ function myFunctiona() {
     alert("Are You Sure!?");
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let divimages = document.querySelector('.row');
+
+    let imagesSrc = ["images/demo/food.jpg", "images/demo/ملابس.jpeg", "images/demo/مصاري.jpg", "images/demo/other.jpg", "images/demo/services.jpg", "images/demo/blood.png"];
+
+    fillImages('row', imagesSrc);
+
+    function fillImages(name, imagesList) {
+
+      let images = document.querySelector('.' + name);
+
+      imagesList.forEach(function (option) {
+
+        let image = document.createElement('img');
+
+        image.src = option;
+
+        image.classList='container, column';
+
+        images.appendChild(image);
+        
+      });
+    }
+  });
